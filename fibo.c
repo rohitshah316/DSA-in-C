75e773cc7027c5698e5b0ff4a10b876afa40c8d8
Rohit Shah <shahrohit2223@gmail.com>
2026-03-13 19:35:54 +0545

    fibonacci series

---
+++ fibo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int fibo(int n){
+    if(n==0){
+        return 0;
+    }else if(n==1){
+        return 1;
+    }else{
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+
+int main(){
+
+    int n;
+    printf("Enter num of terms:");
+    scanf("%d",&n);
+
+
+    printf("Fibonacci Series:\n");
+    for(int i=0;i<n;i++){
+        printf("%d ", fibo(i));
+    }
+
+    return 0;
+}
